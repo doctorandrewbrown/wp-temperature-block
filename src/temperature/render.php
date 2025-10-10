6291ad03f47aa22c1b58f47a1b6bc1f8c2befc88
Andy Brown <dr.andrew.david.brown@gmail.com>
2025-10-10 22:56:05 +0100

    implement temperature conversion

--- src/temperature/render.php
+++ src/temperature/render.php
@@ -1,8 +1,5 @@
-<?php
-/**
- * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
- */
-?>
-<p <?php echo get_block_wrapper_attributes(); ?>>
-	<?php esc_html_e( 'Temperature – hello from a dynamic block!', 'temperature' ); ?>
-</p>
+<form action="" class="alignfull temp-form">
+	<label for="celsius">Enter degrees C temperature:</label>
+    <input type="number" id="celsius">
+	<span id="result"></span>
+</form>
--- src/temperature/view.js
+++ src/temperature/view.js
@@ -1,27 +1,9 @@
-/**
- * Use this file for JavaScript code that you want to run in the front-end
- * on posts/pages that contain this block.
- *
- * When this file is defined as the value of the `viewScript` property
- * in `block.json` it will be enqueued on the front end of the site.
- *
- * Example:
- *
- * ```js
- * {
- *   "viewScript": "file:./view.js"
- * }
- * ```
- *
- * If you're not making any changes to this file because your project doesn't need any
- * JavaScript running in the front-end, then you should delete this file and remove
- * the `viewScript` property from `block.json`.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-metadata/#view-script
- */
 
-/* eslint-disable no-console */
-console.log( 'Hello World! (from create-block-temperature block)' );
-/* eslint-enable no-console */
-
-alert("hellooooo")
+document.getElementById("celsius").addEventListener("input", convertTemp);
+const result = document.getElementById("result");
+function convertTemp(){
+    const celsius = document.getElementById("celsius").value;
+    const fahrenheit = celsius*1.8+32;
+    result.innerText = fahrenheit;
+}
+  
