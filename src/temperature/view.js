30eea8c907c294ed266a454addb83dc50edc6380
Andy Brown <dr.andrew.david.brown@gmail.com>
2025-10-11 14:59:11 +0100

    refactor function

--- src/temperature/view.js
+++ src/temperature/view.js
@@ -1,14 +1,14 @@
-// get all instances of the block to loop through
-document.querySelectorAll(".temp-form").forEach(convertTemp)
+// get all instances of the block to loop through and apply function
+document.querySelectorAll(".block-wrapper").forEach(convertTemp)
 function convertTemp(block){
     block.querySelector(".celsius").addEventListener("input", ()=>{
+
     // do conversion to fahrenheit
     const celsius = block.querySelector(".celsius").value;
     const fahrenheit = celsius*1.8+32;
-    console.log(fahrenheit);
-    const result = block.querySelector(".result");
+    
     // generate string to show fahrenheit result (2 sf.) in the span element
-    result.innerText = `  =  ${fahrenheit.toFixed(2)} degrees F`;
+    block.querySelector(".result").innerText = `  =  ${fahrenheit.toFixed(2)} degrees F`;
     });
 };
 
