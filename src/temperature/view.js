cb2b1213fd68e0e2c6dc5dd1ee3440ebfbfcddca
Andy Brown <dr.andrew.david.brown@gmail.com>
2025-10-11 11:26:17 +0100

    add comment

--- src/temperature/view.js
+++ src/temperature/view.js
@@ -3,7 +3,7 @@ document.getElementById("celsius").addEventListener("input", ()=>{
     // do conversion to fahrenheit
     const celsius = document.getElementById("celsius").value;
     const fahrenheit = celsius*1.8+32;
-    // generate string to show fahrenheit result in the span element
+    // generate string to show fahrenheit result (2 sf.) in the span element
     result.innerText = `  =  ${fahrenheit.toFixed(2)} degrees F`;
 });
 // get #result element in variable
